b536c32f408632afccc843324e32d97c6c661e90
Nouman Sharif <noumansharifgul27@gmail.com>
2024-07-28 23:11:20 +0500

    learning about custom sursor animation

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cursor follower
+
+**In this coding practice we will create a amazing cursor animation as well as understand**
+
+    **lerp (Lenear Inter Polation)**: (0, 80, 0.5)
+    0  : initilize value
+    80 : final value
+    0.5: third Parameter (it is important because on the bases of this we will get the required value. e.g. ans of the above example is 40)
+
+<hr />
+
+    Clamping :
---
+++ app.js
@@ -0,0 +1,100 @@
+let circle = document.querySelector("#circle"); //
+let frames = document.querySelector("#frames");
+// let frame = document.querySelector(".frame");
+
+window.addEventListener("mousemove", (details) => {
+  gsap.to("#circle", {
+    x: details.clientX,
+    y: details.clientY,
+    duration: 0.3,
+    ease: Expo,
+  });
+});
+
+frame.addEventListener("mousemove", () => {
+  let dims = frame.getBoundingClientRect(); //it will gives us the dimensions
+  let start = dims.x;
+  console.log(start);
+  let end = dims.x + dims.width; // as we know the starting point and the width so we are able to initilize the ending point
+  console.log(end);
+  gsap.to("#circle", {
+    scale: 16,
+    duration: 0.3,
+    ease: Expo,
+  });
+
+  gsap.to(".frame span", {
+    color: "#fff",
+    duration: 0.3,
+    ease: Expo,
+  });
+
+  gsap.to(".frame span", {
+    y: "-100%",
+    duration: 0.3,
+    ease: Expo,
+  });
+});
+
+frame.addEventListener("mouseleave", () => {
+  gsap.to("#circle", {
+    scale: 1,
+    duration: 0.3,
+    ease: Expo,
+  });
+
+  gsap.to(".frame span", {
+    color: "#000",
+    duration: 0.3,
+    ease: Expo,
+  });
+
+  gsap.to(".frame span", {
+    y: 0,
+    duration: 0.3,
+    ease: Expo,
+  });
+});
+
+// frames.forEach((frame) => {
+
+//   frame.addEventListener("mousemove", () => {
+//     gsap.to("#circle", {
+//       scale: 12,
+//       duration: 0.3,
+//       ease: Expo,
+//     });
+
+//     gsap.to(frame.children, {
+//       color: "#fff",
+//       duration: 0.3,
+//       ease: Expo,
+//     });
+
+//     gsap.to(frame.children, {
+//       y: "-100%",
+//       duration: 0.3,
+//       ease: Expo,
+//     });
+//   });
+
+//   frame.addEventListener("mouseleave", () => {
+//     gsap.to("#circle", {
+//       scale: 1,
+//       duration: 0.3,
+//       ease: Expo,
+//     });
+
+//     gsap.to(frame.children, {
+//       color: "#000",
+//       duration: 0.3,
+//       ease: Expo,
+//     });
+
+//     gsap.to(frame.children, {
+//       y: 0,
+//       duration: 0.3,
+//       ease: Expo,
+//     });
+//   });
+// });
